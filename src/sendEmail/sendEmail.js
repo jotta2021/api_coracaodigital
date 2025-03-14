ce1a264539210904d9fec6c6a1eed4a1ba39fdf0
jotta2021 <joanderson2572@gmail.com>
2025-03-14 16:34:45 -0300

    ajuste email

--- src/sendEmail/sendEmail.js
+++ src/sendEmail/sendEmail.js
@@ -1,32 +1,42 @@
-import { Request, Response } from "express";
-import nodemailer from "nodemailer";
-import qrcode from 'qrcode'
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true for port 465, false for other ports
-  auth: {
-    user: "coracaovirtualsite@gmail.com",
-    pass: "wawq vrdn rorb btir",
-  },
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const nodemailer_1 = __importDefault(require("nodemailer"));
+const transporter = nodemailer_1.default.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for port 465, false for other ports
+    auth: {
+        user: "coracaovirtualsite@gmail.com",
+        pass: "wawq vrdn rorb btir",
+    },
 });
-
 class SendEmail {
-  async handle(req: Request, res: Response) {
-    //recebe o email do usuario
-    //e link do seu site
-    const { email, link } = req.body;
-
-    try {
-        //gera o qrCode
-        const qrCodeImage = `https://api.qrserver.com/v1/create-qr-code/?data=${link}&size=150x150`
-        
-      const info = await transporter.sendMail({
-        from: '"Coração Digital" <coracaovirtualsite@gmail.com>', // sender address
-        to: email, // list of receivers
-        subject: "Site Coração Digital", // Subject line
-        text: "Hello world?", // plain text body
-        html: `
+    handle(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            //recebe o email do usuario
+            //e link do seu site
+            const { email, link } = req.body;
+            try {
+                //gera o qrCode
+                const qrCodeImage = `https://api.qrserver.com/v1/create-qr-code/?data=${link}&size=150x150`;
+                const info = yield transporter.sendMail({
+                    from: '"Coração Digital" <coracaovirtualsite@gmail.com>', // sender address
+                    to: email, // list of receivers
+                    subject: "Site Coração Digital", // Subject line
+                    text: "Hello world?", // plain text body
+                    html: `
               <body style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
    <div style="max-width: 600px; margin: 0 auto;  padding: 0; background-color: #f9f9f9; ">
       
@@ -62,13 +72,13 @@ class SendEmail {
     </div>
 </body>
               `,
-      })
-
-      res.status(200).json("Email enviado com sucesso")
-    } catch (error) {
-       res.status(500).json(error);
+                });
+                res.status(200).json("Email enviado com sucesso");
+            }
+            catch (error) {
+                res.status(500).json(error);
+            }
+        });
     }
-  }
 }
-
-export default SendEmail;
+exports.default = SendEmail;
